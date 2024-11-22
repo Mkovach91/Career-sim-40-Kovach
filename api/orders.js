5988f44de5d279e26e8f4be0869a20d4398a34fe
matt k <matt.kovach91@gmail.com>
2024-11-22 12:14:19 -0600

    finsihed orders.js and updated .http

--- api/orders.js
+++ api/orders.js
@@ -8,9 +8,7 @@ router.get("/", authenticate, async (req, res, next)=> {
   try {
     const orders = await prisma.order.findMany({
       where: { customerId: req.user.id },
-      include: {
-        items: true,
-      },
+      include: {items: true },
     });
     res.json(orders)
   } catch (error) {
@@ -18,6 +16,45 @@ router.get("/", authenticate, async (req, res, next)=> {
   }
 });
 
+router.post("/", authenticate, async (req, res, next) =>{
+  const { date, note, productIds } = req.body;
 
+  try {
+    const order = await prisma.order.create({
+      data: {
+        date,
+        note,
+        customerId: req.user.id,
+        items: {
+          create: productIds.map((productId) => ({
+            product: { connect: { id: +productId } }
+          })),
+        },
+      },
+    });
+    res.status(201).json(order);
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.get("/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params
+  
+  try {
+    const order = await prisma.order.findUniqueOrThrow({
+      where: { id: +id },
+      include: { items: { include: { product: true } } },
+    });
+
+    if(order.customerId !== req.user.id) {
+      throw { status: 403, message: "You do not own this order."};
+    }
+
+    res.json(order);
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
