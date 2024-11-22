58649b859334d9e6aac4035d7ed6c79376eba07a
matt k <matt.kovach91@gmail.com>
2024-11-22 10:45:14 -0600

    Created Index.js for prisma import and Seeded database

---
+++ prisma/seed.js
@@ -0,0 +1,19 @@
+const prisma = require("../prisma");
+
+const seed = async (numProducts = 20) => {
+  const products = Array.from({ length: numProducts }, (_, i) => ({
+    title: `Product ${i + 1}`,
+    description: `Description for product ${i + 1}`,
+    price: (i + 1) * 10,
+  }));
+
+  await prisma.product.createMany({ data: products });
+};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
